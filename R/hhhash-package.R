cb480202f1d8ea5ed744de19e5d2e5433aaa2ca0
hrbrmstr <bob@rud.is>
2023-07-09 06:10:45 -0400

    R 📦 repo initialization complete

---
+++ R/hhhash-package.R
@@ -0,0 +1,14 @@
+# nolint start
+
+#' ...
+#' 
+#' @md
+#' @name hhhash
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+# @import httr
+# @importFrom jsonlite fromJSON
+# @ useDynLib hhhash, .registration = TRUE
+"_PACKAGE"
+
+# nolint end
