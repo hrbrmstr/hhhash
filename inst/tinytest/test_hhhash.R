c8c5873f25cb68e3830b19f49977b7f0c283682e
hrbrmstr <bob@rud.is>
2023-07-09 07:26:37 -0400

    chore: tests and badge

--- inst/tinytest/test_hhhash.R
+++ inst/tinytest/test_hhhash.R
@@ -1,4 +1,14 @@
+library(hhhash)
 
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
+c(
+  "Date", "Server", "Strict-Transport-Security",
+  "Last-Modified", "ETag", "Accept-Ranges",
+  "Content-Length", "Content-Security-Policy",
+  "X-Content-Type-Options", "X-Frame-Options",
+  "X-XSS-Protection", "Content-Type"
+) -> keys # nolint: assignment_linter.
 
+expect_equal(
+  hash_headers(keys),
+  "hhh:1:78f7ef0651bac1a5ea42ed9d22242ed8725f07815091032a34ab4e30d3c3cefc"
+)
